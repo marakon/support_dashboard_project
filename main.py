f76443a4de22eaef3a0c254bb526afa2676c31ff
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2019-05-21 23:26:10 +0200

    created basic app structure

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import call_zendesk
 import handler_zendesk
 import json_zendesk
+import eel
+
+eel.init("web")
 
 test_call = call_zendesk.Call().unassignedQueue()
 
 test_response = json_zendesk.JsonOperation(test_call).load()
 
-print(handler_zendesk.DataHandler(test_response).viewTickets())
+@eel.expose
+def ticket_count():
+    return handler_zendesk.DataHandler(test_response).count
+
+eel.start("index.html", size=(600, 600))
