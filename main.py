7e52605a129c74e834273e49f9a401ce1eb7fe8b
heheszek <mateuszosinski96@gmail.com>
2019-05-23 16:58:07 +0200

    main changes

--- main.py
+++ main.py
@@ -7,35 +7,31 @@ import eel
 
 eel.init("web")
 
-unassigned_call = call_module.ZendeskCall()
-unassigned_handle = handler_module.Calculate()
-unassigned_raw = unassigned_call.unassignedQueue
-unassigned = json_module.JsonOperation(unassigned_raw)
-unassigned_loaded = unassigned.load()
-(unassigned_handle.tickets, unassigned_handle.ticketCount, unassigned_handle.platinumCount, unassigned_handle.premiumCount) = (unassigned_loaded, unassigned_loaded, unassigned_loaded, unassigned_loaded)
+def unassigned():
+    unassigned_call = call_module.ZendeskCall()
+    unassigned_handle = handler_module.Calculate()
+    unassigned_raw = unassigned_call.unassignedQueue
+    unassigned = json_module.JsonOperation(unassigned_raw)
+    unassigned_loaded = unassigned.load()
+    (unassigned_handle.tickets, unassigned_handle.ticketCount, unassigned_handle.platinumCount, unassigned_handle.premiumCount) = (unassigned_loaded, unassigned_loaded, unassigned_loaded, unassigned_loaded)
+    return unassigned_handle.tickets, unassigned_handle.ticketCount, unassigned_handle.platinumCount, unassigned_handle.premiumCount
 
 #==========================================================================================#
 # Variables
 
-# Number of premium cases
-premium = unassigned_handle.premiumCount
-
-# Number of platinum cases
-platinum = unassigned_handle.platinumCount
+(un_tickets, un_ticketsCount, un_platinum, un_premium) = unassigned()
 
 # Premium and platinum together
-prem_and_plat = premium + platinum
+prem_and_plat = un_premium + un_platinum
 
 # List of unassigned queue
-unassignedView = unassigned_handle.unassignedView()
+unassignedView = un_tickets.unassignedView()
 
-# Number of tickets in unassigned
-ticketsCount = unassigned_handle.ticketCount
 #==========================================================================================#
 
 
 @eel.expose
 def ticket_count():
-    return unassignedView
+    return un_ticketsCount
 
 eel.start("index.html", size=(600, 600))
